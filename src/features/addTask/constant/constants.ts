c0e09ddf84dd55734b59c397abfd28b7afa8e15b
Shilov Stas <sofabb9@gmail.com>
2024-10-14 23:24:58 +0700

    add features

---
+++ src/features/addTask/constant/constants.ts
@@ -0,0 +1,10 @@
+/** Список ошибок валидации. */
+export enum PromptText {
+	EMPTY_FIELD = 'Сначала заполните все поля',
+	NOT_AVAILABLE_TYPE = 'Нельзя создать задачу для списка завершенных задач',
+}
+
+export enum LabelText {
+	DESCRIPTION = 'Введите описание задачи',
+	TITLE = 'Введите название задачи'
+}
---
+++ src/features/taskContext/index.ts
@@ -0,0 +1,1 @@
+export {TaskSetterContext, TaskTypeContext} from './TaskContext';
