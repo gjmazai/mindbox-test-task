b91a7ea1b9db1a92b965b144df391f4ad6532551
Shilov Stas <sofabb9@gmail.com>
2024-10-14 23:25:12 +0700

    add shared

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * @file Модуль общие экспортирует константы приложения.
+ * 
+ * @author gjmazai
+ */
+
+/**
+ * Ключ по которому будут сохранятся записи в локальном хранилище.
+ * @constant
+ */
+export const STORAGE_KEY = 'mindBoxTodoList';
+
+/**
+ * Ключ по которому будет сохранятся последний идентификатор в локальном хранилище.
+ * @constant
+ */
+export const UNIQ_ID_KEY = 'mindBoxLastId';
